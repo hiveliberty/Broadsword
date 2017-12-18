aa79f9dfda5e2a139bea48512ba645d97eba64a4
hiveliberty <cyberlibertyx@gmail.com>
2017-12-18 15:26:13 +0400

    fixing the loop in the mail_check and some other fixes

--- install/install_debian9.sh
+++ install/install_debian9.sh
@@ -2,7 +2,14 @@
 #
 
 apt update
-apt install python3 python3-setuptools python3-lxml
+
+apt install git curl
+apt install apache2 libapache2-mpm-itk
+apt install php php-curl php-cli
+
+apt install mariadb-server
+
+apt install python3-setuptools python3-lxml
 apt install python3-wheel python3-pip --no-install-recommends
 
 python3 -m pip -U discord.py
--- modules/mail.py
+++ modules/mail.py
@@ -44,21 +44,16 @@ class EVEMail:
 
                 if self.time_next is not None:
                     self.time_next = datetime.datetime.fromtimestamp(float(self.time_next))
-                    self.time_now = datetime.datetime.now().replace(microsecond=0)
-
-                    if self.time_next <= self.time_now:
+                    #if self.time_next <= await self.now():
+                    if await self.now() >= self.time_next:
+                        log.info("Start periodic check corp\\alliance mails.")
                         if config.bot["devMode"]:
                             print("Start periodic check corp\\alliance mails.")
-                        else:
-                            log.info("Start periodic check corp\\alliance mails.")
                         await self.mail_check()
                 else:
-                    self.time_now = datetime.datetime.now().replace(microsecond=0)
-                    self.time_next = self.time_now + datetime.timedelta(seconds=config.evemails["check_interval"])
-                    self.time_next = time.mktime(self.time_now.timetuple())
-                    await self.cnx.storage_add("nextMailCheck", self.time_next)
-
-                del self.cnx
+                    await self.cnx.storage_add("nextMailCheck", await self.next_check(self.interval))
+                for attr in ("cnx", "t_next", "t_now"):
+                    self.__dict__.pop(attr,None)
                 await asyncio.sleep(self.interval)
         except Exception as e:
             if config.bot["devMode"]:
@@ -76,65 +71,74 @@ class EVEMail:
 
     async def mail_check(self):
         try:
-            while not self.broadsword.is_closed:
-                self.cnx = DBMain()
-                self.latestMailID = await self.cnx.storage_get("latestMailID")
-                if self.latestMailID is None:
-                    self.latestMailID = "0"
-                self.maxID = self.latestMailID
-                self.url = "https://api.eveonline.com/char/MailMessages.xml.aspx?keyID={0}&vCode={1}&characterID={2}" \
-                            .format(config.evemails["keyID"], config.evemails["vCode"], config.evemails["characterID"])
-                self.mails = await EVEBasic.make_api_request(self.url)
-                self.mails = await MailUtils.xml_to_dict(self.mails, "mails")
-                if self.mails is not None:
-                    for self.mail in self.mails:
-                        if (self.mail["@toCorpOrAllianceID"] in config.evemails["fromIDs"]) and (self.mail["@messageID"] > self.maxID):
-                            self.url = "https://api.eveonline.com/char/MailBodies.xml.aspx?keyID={0}&vCode={1}&characterID={2}&ids={3}"\
-                                            .format(config.evemails["keyID"],\
-                                                    config.evemails["vCode"],\
-                                                    config.evemails["characterID"],\
-                                                    self.mail["@messageID"]\
-                                                    )
-                            self.content = await EVEBasic.make_api_request(self.url)
-                            self.content = await MailUtils.xml_to_dict(self.content, "content")
-                            if self.content is not None:
-                                self.content = await MailUtils.format_mailbody(self.content)
-                                self.msg_split = await MailUtils.str_split(self.content, 1800)
+            self.cnx = DBMain()
+            self.latestMailID = await self.cnx.storage_get("latestMailID")
+            if self.latestMailID is None:
+                self.latestMailID = "0"
+            self.maxID = self.latestMailID
+            self.url = "https://api.eveonline.com/char/MailMessages.xml.aspx?keyID={0}&vCode={1}&characterID={2}" \
+                        .format(config.evemails["keyID"], config.evemails["vCode"], config.evemails["characterID"])
+            self.mails = await EVEBasic.make_api_request(self.url)
+            self.mails = await MailUtils.xml_to_dict(self.mails, "mails")
+            if self.mails is not None:
+                for self.mail in self.mails:
+                    if (self.mail["@toCorpOrAllianceID"] in config.evemails["fromIDs"]) and (self.mail["@messageID"] > self.maxID):
+                        self.url = "https://api.eveonline.com/char/MailBodies.xml.aspx?keyID={0}&vCode={1}&characterID={2}&ids={3}"\
+                                        .format(config.evemails["keyID"],\
+                                                config.evemails["vCode"],\
+                                                config.evemails["characterID"],\
+                                                self.mail["@messageID"]\
+                                                )
+                        self.content = await EVEBasic.make_api_request(self.url)
+                        self.content = await MailUtils.xml_to_dict(self.content, "content")
+                        if self.content is not None:
+                            self.content = await MailUtils.format_mailbody(self.content)
+                            self.msg_split = await MailUtils.str_split(self.content, 1800)
 
-                                self.msg = "**------------------------------------**\n"
-                                self.msg += "**Mail By: **{}\n".format(self.mail["@senderName"])
-                                self.msg += "**Sent Date: **{}\n".format(self.mail["@sentDate"])
-                                self.msg += "**Title: ** {}\n".format(self.mail["@title"])
-                                self.msg += "**Content: **\n"
-                                self.msg += self.msg_split[0]
+                            #self.msg = "**------------------------------------**\n"
+                            self.msg = "**-------------Start Mail-------------**\n"
+                            self.msg += "**Mail By: **{}\n".format(self.mail["@senderName"])
+                            self.msg += "**Sent Date: **{}\n".format(self.mail["@sentDate"])
+                            self.msg += "**Title: ** {}\n".format(self.mail["@title"])
+                            self.msg += "**Content: **\n"
+                            self.msg += self.msg_split[0]
 
-                                await self.cnx.message_add(self.msg, self.channel)
-                                if len(self.msg_split) > 0:
-                                    for self.i in range(1, len(self.msg_split)):
-                                        await self.cnx.message_add(self.msg_split[self.i], self.channel)
-                                self.maxID = max(self.mail["@messageID"], self.maxID)
-                    #print("Latest mailID is {}".format(self.maxID))
-                    if self.maxID > self.latestMailID:
-                        await self.cnx.storage_add("latestMailID", self.maxID)
+                            await self.cnx.message_add(self.msg, self.channel)
+                            if len(self.msg_split) > 0:
+                                for self.i in range(1, len(self.msg_split)):
+                                    await self.cnx.message_add(self.msg_split[self.i], self.channel)
 
-                    self.time_now = datetime.datetime.now().replace(microsecond=0)
-                    self.time_next = self.time_now + datetime.timedelta(seconds=config.evemails["check_interval"])
-                    self.time_next = time.mktime(self.time_next.timetuple())
-                    await self.cnx.storage_update("nextMailCheck", self.time_next)
-                else:
-                    if config.bot["devMode"]:
-                        print("EVE ESI is unavailable.")
-                    else:
-                        log.warning("EVE ESI is unavailable.")
+                            self.endmsg = "**--------------End Mail--------------**\n"
+                            await self.cnx.message_add(self.endmsg, self.channel)
+
+                            self.maxID = max(self.mail["@messageID"], self.maxID)
+
+                if self.maxID > self.latestMailID:
+                    await self.cnx.storage_add("latestMailID", self.maxID)
+            else:
+                log.warning("EVE Api service is unavailable.")
+                if config.bot["devMode"]:
+                    print("EVE Api service is unavailable.")
+            await self.cnx.storage_update("nextMailCheck", await self.next_check(self.interval))
+            return 0
         except Exception as e:
             if config.bot["devMode"]:
                 print(e)
             log.exception("An exception has occurred in {}: ".format(__name__))
         finally:
-            for attr in ("cnx", "latestMailID", "maxID", "url",
+            for attr in ("latestMailID", "maxID", "url",
                          "mails", "mail", "content", "msg_split",
                          "msg", "time_now", "time_next"):
                 self.__dict__.pop(attr,None)
+
+    async def next_check(self, interval):
+        self.t_next = await self.now() + datetime.timedelta(seconds=interval)
+        self.t_next = time.mktime(self.t_next.timetuple())
+        return self.t_next
+
+    async def now(self):
+        self.t_now = datetime.datetime.now().replace(microsecond=0)
+        return self.t_now
 
 
 def setup(broadsword):
